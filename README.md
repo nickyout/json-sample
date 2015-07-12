520fb805f5b8d7a1eae334cab6e347102ce4e3df
Nicky Out <nickyout@gmail.com>
2015-07-12 15:23:47 +0200

    API sync seems to work.

---
+++ README.md
@@ -0,0 +1,2 @@
+## How to add
+For github urls, please use https://cdn.rawgit.com or some other cdn and target the json file using a specific commit, *not* a branch like 'master'. 
--- index.js
+++ index.js
@@ -6,15 +6,72 @@ var fse = require('fs-extra'),
 	extractStats = require('./lib/registry/extract-stats'),
 	jsonDownload = require('./lib/util/json-download');
 
-var log = console.log.bind(console, "json-sample:");
+var log = console.log.bind(console, "json-sample:"),
+	logDone = function(numErrors) {
+		if (typeof numErrors === "number") {
+			log("Done ("+ numErrors + " error"+(numErrors===1?"":"s")+").")
+		} else {
+			log("Done");
+		}
+	};
 
 var localRegistryPath = path.resolve(__dirname, 'registry.json'),
 	localRegistry = new JSONSampleRegistry(localRegistryPath);
 
 module.exports = {
 
-	sync: function() {
-
+	sync: function(force) {
+		var remoteNames = [],
+			numErrors = 0;
+		return localRegistry.read()
+			.then(function(result) {
+				var remotes = result.remotes,
+					downloads = [],
+					remoteName;
+				for (remoteName in remotes) {
+					remoteNames.push(remoteName);
+					downloads.push(
+						jsonDownload(remotes[remoteName])
+							.catch(function(err) {
+								numErrors++;
+								log('failed to read registry: "' + err.message + '". Skipping...');
+							})
+					);
+				}
+				return Promise.all(downloads);
+			})
+			.then(function(results) {
+				var merges = [],
+					i;
+				for (i = 0; i < results.length; i++) {
+					if (results[i]) {
+						merges.push(
+							localRegistry.merge(results[i].obj, force)
+								.catch(function(err) {
+									numErrors++;
+									log('failed to sync with registry:"' + err.message + '". Skipping...');
+								}));
+					}
+				}
+				return Promise.all(merges);
+			})
+			.then(function(results) {
+				var i, totalStats = {
+					added: 0,
+					updated: 0
+				};
+				for (i = 0; i < results.length; i++) {
+					if (results[i]) {
+						totalStats.added += results[i].added;
+						totalStats.updated += results[i].updated;
+					}
+				}
+				log("sync complete: " + totalStats.added + " added, " + totalStats.updated + " updated.");
+			})
+			.then(localRegistry.write)
+			.done(function() {
+				logDone(numErrors);
+			});
 	},
 
 	add: function(name, url, tags, force) {
@@ -37,10 +94,16 @@ module.exports = {
 				throw err;
 			})
 			.then(localRegistry.write)
-			.done();
+			.then(function() {
+				log("sample " + name + " added to registry.");
+			})
+			.done(function() {
+				logDone();
+			});
 	},
 
 	install: function() {
+		var numErrors = 0;
 		Promise.all([readJSON('./json-samples.json'), localRegistry.read()])
 			.catch(function(err) {
 				log("Could not read json-samples: "+ err.message);
@@ -62,6 +125,7 @@ module.exports = {
 									log("saved to " + result.path);
 								})
 								.catch(function(err) {
+									numErrors++;
 									log("failed to create: \"" + err.message + "\". Skipping...");
 								})
 						);
@@ -70,7 +134,7 @@ module.exports = {
 				return Promise.all(downloadables);
 			})
 			.done(function(results){
-				log("Done.");
+				logDone(numErrors);
 			});
 	},
 
