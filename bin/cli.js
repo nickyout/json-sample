76c323f187abac39ab5eca49c3b69b98143817c0
Nicky Out <nickyout@gmail.com>
2015-07-12 14:02:16 +0200

    API add and install seem to work.

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,6 @@
-var argv = require('minimist')(process.argv.slice(2)),
-	jsonSample = require('../lib/registry/index');
+var argv = require('minimist')(process.argv.slice(2));
 if (argv._.length > 0) {
-	jsonSample.download(argv._[0], argv._[1], function(err, path) {
-		if (err) {
-			console.error("Error: " + err.message);
-		} else {
-			console.log("Written to ", path);
-			console.log(path);
-		}
-	});
+
 } else {
 	console.log("Usage: json-samples <url> [<dest>]");
 	process.exit(1);
--- index.js
+++ index.js
@@ -3,16 +3,13 @@ var fse = require('fs-extra'),
 	Promise = require('promise'),
 	readJSON = Promise.denodeify(fse.readJSON),
 	JSONSampleRegistry = require('./lib/registry/class'),
+	extractStats = require('./lib/registry/extract-stats'),
 	jsonDownload = require('./lib/util/json-download');
 
-var log = console.log.bind(console);
+var log = console.log.bind(console, "json-sample:");
 
-var rootDir = __dirname,
-	localRegistryPath = path.resolve(rootDir, 'registry.json');
-
-var localRegistry = new JSONSampleRegistry(localRegistryPath),
-	// Temporary
-	defaultRemoteRegistry = new JSONSampleRegistry(path.resolve(rootDir, 'remote', 'registry.json'));
+var localRegistryPath = path.resolve(__dirname, 'registry.json'),
+	localRegistry = new JSONSampleRegistry(localRegistryPath);
 
 module.exports = {
 
@@ -20,21 +17,61 @@ module.exports = {
 
 	},
 
-	install: function(target, doSave) {
-		if (!target) {
-			// install all
-			Promise.all([readJSON('./json-samples.json'), localRegistry.read()])
-				.then(function(results) {
-
-				})
-				.catch(function(err) {
-					log("Could not read json-samples: "+ err.message);
-				});
-		}
+	add: function(name, url, tags, force) {
+		return jsonDownload(url)
+			.catch(function(err) {
+				log("Could not download JSON: \"" + err.message + "\"");
+				throw err;
+			})
+			.then(function(result) {
+				return localRegistry.add(name, {
+					url: url,
+					date: +new Date(),
+					tags: tags || [],
+					numBytes: result.numBytes,
+					stats: extractStats(result.obj)
+				}, force);
+			})
+			.catch(function(err) {
+				log("Could not create new entry: \"" + err.message + "\"");
+				throw err;
+			})
+			.then(localRegistry.write)
+			.done();
 	},
 
-	remove: function(target, doSave) {
+	install: function() {
+		Promise.all([readJSON('./json-samples.json'), localRegistry.read()])
+			.catch(function(err) {
+				log("Could not read json-samples: "+ err.message);
+				throw err;
+			})
+			.then(function(results) {
+				var requestedJSON = results[0],
+					registrySamples = results[1].samples,
+					downloadables = [],
+					jsonName;
 
+				for (jsonName in requestedJSON) {
+					if (!registrySamples.hasOwnProperty(jsonName)) {
+						log("sample " + jsonName + " not present in registry. Skipping...");
+					} else {
+						downloadables.push(
+							jsonDownload(registrySamples[jsonName].url, requestedJSON[jsonName])
+								.then(function(result) {
+									log("saved to " + result.path);
+								})
+								.catch(function(err) {
+									log("failed to create: \"" + err.message + "\". Skipping...");
+								})
+						);
+					}
+				}
+				return Promise.all(downloadables);
+			})
+			.done(function(results){
+				log("Done.");
+			});
 	},
 
 	search: function(grepStr, options) {
